74855c3f0814ca2505c15f0285f61591f2547415
Harun ELALTUNKARA <harunelaltunkara@gmail.com>
2025-04-22 17:25:43 +0300

    temel mekanikler

---
+++ Mr.Homeless2.1/Assets/Scripts/Building.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public string buildingName = "Ýsimsiz Bina";
+    public static Building LastVisitedBuilding; // Son ziyaret edilen bina
+    public BuildingUI timeAdd;
+
+    void OnMouseDown()      // Git butonuna basýnca çalýþan fonksiyon
+    {
+        float travelTime = 0f;
+        if (LastVisitedBuilding != null && LastVisitedBuilding != this)
+        {
+            float distance = Vector3.Distance(transform.position, LastVisitedBuilding.transform.position);
+            if (distance > 0 && distance <= 10f)
+                travelTime = 30f; // 30 dakika
+            else if (distance > 10 && distance <= 20f)
+                travelTime = 60f; // 60 dakika
+            else if (distance > 20 && distance <= 30f)
+                travelTime = 90f; // 1.5 saat
+            else
+                travelTime = 120f; // 2 saat
+        }
+        BuildingUI.Instance.Show(buildingName, travelTime, () =>   
+        { 
+            Debug.Log($"{buildingName} için iþlem baþlatýldý!");
+            timeAdd.Clock(travelTime);
+            LastVisitedBuilding = this; // Þu anki binayý "son gidilen" olarak kaydet
+        }); 
+    }
+}
---
+++ Mr.Homeless2.1/Assets/Scripts/BuildingUI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class BuildingUI : MonoBehaviour
+{  
+    private float currentTime = 480f;
+    public static BuildingUI Instance;
+    
+    public TMP_Text clockText;
+    public GameObject BuildingPanel, PauseMenuPanel;
+    public TMP_Text nameText, timeText;
+    public Button actionButton;
+
+    void Awake() => Instance = this;
+
+    public void Clock(float time)   //Binalar arası seyahatta geçen süreyi gösterir
+    {
+        currentTime += time;
+        int totalSeconds = Mathf.FloorToInt(currentTime);
+        int hours = (totalSeconds / 60) % 24;
+        int minutes = totalSeconds % 60;
+        clockText.text = $"{hours:D2}:{minutes:D2}";
+    }
+
+    public void Show(string name, float time, Action onClick)   //Binaya tıkladığında bina adını ve bilgilerini gösteren paneli açar
+    {
+        nameText.text = name;
+        timeText.text = $"Varış süresi: {time} saniye";
+
+        actionButton.onClick.RemoveAllListeners();
+        actionButton.onClick.AddListener(() =>
+        {
+            onClick?.Invoke();
+            BuildingPanel.SetActive(false);
+        });
+        BuildingPanel.SetActive(true);
+    }
+
+    public void Pause() { Time.timeScale = 0f; PauseMenuPanel.SetActive(true); }
+    public void Resume() { Time.timeScale = 1f; PauseMenuPanel.SetActive(false); }
+}
